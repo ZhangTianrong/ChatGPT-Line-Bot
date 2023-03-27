1e5a3021976f90e0a690f18da81fed2c67787448
Tianrong Zhang <zhangtianrongunique@outlook.com>
2023-03-27 23:39:54 +0000

    local changes

--- main.py
+++ main.py
@@ -193,4 +193,4 @@ if __name__ == "__main__":
             model_management[user_id] = OpenAIModel(api_key=data[user_id])
     except FileNotFoundError:
         pass
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
